0da3de3a39d2e668bb235e337bb44b8ca3f81e38
Louis Koh <louiskoh1@gmail.com>
2022-09-17 14:43:53 +0800

    day3b

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './components/login/Login';
 import Register from './components/register/Register';
 import Profile from './components/profile/Profile';
 import EditProfile from './components/profile/EditProfile';
+import UploadPhoto from './components/profile/UploadPhoto';
 import Auth from './components/auth/Auth';
 import Guest from './components/auth/Guest'
 
@@ -19,7 +20,7 @@ function App() {
         <Route path="/" element={<Animals />} />
         <Route path="/profile" element={<Auth component={Profile} />} />
         <Route path="/editprofile" element={<Auth component={EditProfile} />} />
-        {/* <Route path="/animals/:animalId/edit" element={<Auth component={EditAnimalDetails} />} /> */}
+        <Route path="/uploadphoto" element={<Auth component={UploadPhoto} />} />
         <Route path="/register" element={<Guest component={Register} />} />
         <Route path="/user/login" element={<Guest component={Login} />} />
       </Routes>
---
+++ src/components/Profile/UploadPhoto.jsx
@@ -0,0 +1,58 @@
+import {useState} from 'react'
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+  
+function UploadPhoto() {
+    const navigate = useNavigate()
+    const [images, setImages] = useState([])
+
+    function onImageChange(e) {
+        setImages(e.target.files[0])
+    }
+
+    function handleFormSubmit(e) {
+        e.preventDefault()
+        console.log('Image submitted to the backend',images)
+        const formData = new FormData();
+        formData.append("uploaded_file", images);
+        console.log('DATA submitted to the backend',formData)
+        const api = 'http://localhost:8000/profile/uploadPhoto'
+        const token = localStorage.getItem('user_token')
+        axios.post(
+                api , 
+                formData,
+                { 
+                    headers: 
+                        {
+                            "Authorization" : `Bearer ${token}`
+                        }
+                }
+                )
+                .then(function (response) {
+                    console.log('RESPONSE',response)
+                    toast(response.data)
+                    navigate('/profile')
+                })
+                .catch(function (error) {
+                    console.log('ERROR',error);
+                    toast(error)
+                })
+}
+
+
+    return (
+        <div>
+            <h1>Upload New Photo</h1>
+            <ToastContainer />
+            <form onSubmit={handleFormSubmit}>
+                <input type="file" onChange={onImageChange}/>
+                <button type="submit" className="btn btn-primary">Upload Photo</button>
+            </form>
+        </div>
+    )
+}
+
+export default UploadPhoto
